665b1c5764a50649aeb58984914a8349725b8187
vaneloyza <vanesa.loyza@gmail.com>
2023-07-03 09:46:21 -0300

    cuanto commit Agregué ,modificar, eliminar, buscar, filtrar, agregar

--- README.txt
+++ README.txt
@@ -1,3 +1,8 @@
 El algoritmo creado para la primera pre-entrega es un silumador de una tienda de compras.
 Descripción del algoritmo:
 Al ajecutar el programa debe dar la bienvenida a la tienda, luego de eso, muestra un menu en prompt con dierentes opciones de grupos de artículos y en base a la opción elegida, utilizando alert muestra el grupo de artículos con sus precios. Si la entrada por prompt no corresponde a ninguna de las opciones del menu, debe dar un mensaje alertando "la opción seleccionada no es la correcta". Si el usuario elige 0 del menu, el programa debe terminar y luego da el mensaje de "Gracias por su visita".
+
+Segunda Pre Entrega
+
+
+"Agregue, un menu administrativo valido por una contraseña que permite agregar, modificar,buscar, eliminar y listar los articulos en el arrarys productos. Además, se modificó el código para el menu principal para que se puedan realizarlas operaciones presentadas en la primera entrega, peron esta vez, utilizando el arreglo productos. Se utilizó filtrado por categorías en el menu pricipal."  
--- javascript.js
+++ javascript.js
@@ -1,105 +1,318 @@
 //Ecommerce de Diseño y Decoración
+
 function  Bienvenida () {
-    alert("Bienvenido a la tienda");
+    alert("Bienvenido a la Tienda");
 }
+
+function contraseña (password) {
+    let clave = prompt ("Ingresar contraseña")
+    if (clave == "1234"){
+        password.password = true;
+        }
+    else if (clave !== "1234"){
+        password.password = false; 
+        }
+}
+
+class producto{
+        
+        constructor(id,nombre,categoria,precio){
+        this.id = id;
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precio = precio;
+        
+        }
+}
+
+function precargararreglo (productos,id) {
+        
+    
+        productos.push(new producto(id.id,"Almohadon 50 x 50","Textil","4000"))
+        productos.push(new producto((id.id=id.id+1),"Manta","Textil","5000"))
+        productos.push(new producto((id.id=id.id+1),"Alfombra","Textil","6500"))
+        productos.push(new producto((id.id=id.id+1),"Luminaria Colgante","Iluminación","10.000"))
+        productos.push(new producto((id.id=id.id+1),"Luminaria de pie","Iluminación","15.000"))
+        productos.push(new producto((id.id=id.id+1),"Luminaria fibras naturales chicas","Iluminación","8.000"))
+        productos.push(new producto((id.id=id.id+1),"Floreros", "Decoración", "2000"))
+        productos.push(new producto((id.id=id.id+1),"Velas", "Decoración", "1500"))
+        productos.push(new producto((id.id=id.id+1),"Macetas","Decoración", "2000"))    
+        productos.push(new producto((id.id=id.id+1),"Cuadros chicos","Cuadros","2.000"))
+        productos.push(new producto((id.id=id.id+1),"Cuadros medianos","Cuadros","3.500"))
+        productos.push(new producto((id.id=id.id+1),"Cuadros grandes","Cuadros","4.500"))
+        
+}
+
+function filtrarProductos(filtro) { 
+
+    resultado = this.productos.filter((producto) => producto.categoria.includes (filtro))
+    console.log(resultado);
+    return resultado;
+    
+}
+function ExisteArticulo(arreglo,existe) { 
+
+    verdadero = arreglo.some((producto) => producto.id == existe)
+    return verdadero;
+    
+}
+
 function Carrito(descripción) {
     alert("Usted ha comprado el siguiente producto\n" + descripción);
     alert("Gracias por su compra");
 }
 
-function menu () {
-    let opción = prompt ("Seleccione la opción deseada:\n 1) Textil \n 2) Iluminación \n 3) Decoración \n 4) Cuadros \n 0) Salir");
-    while (opción != 0) {
-        switch (opción) {
+
+function agregarProductos(id,nombre, categoria, precio) {
+    productos.push(new producto((id.id=id.id+1),nombre,categoria, precio));
+}
+
+
+function verDatos(producto) {
+    return `Codigo Articulo: ${producto.id}
+    Articulo: ${producto.nombre}
+    Categoria: ${producto.categoria}
+    Precio: ${producto.precio}`;
+}
+
+function verProductos(arreglo) {
+    let datosProductos = "Lista de productos: \n \n";
+    arreglo.forEach((producto) => {
+        datosProductos += `${(verDatos(producto))} \n`;
+    });
+    return datosProductos;
+}
+
+
+
+function verProducto(id) {
+    let producto = this.productos.find(producto => producto.id === id);
+    if (producto) {
+        return verDatos(producto);
+    } else {
+        return `No existe producto con ese nombre ${producto.id}`;
+    }
+}
+
+
+function modificarProducto(id,productos) {
+    let producto = this.productos.find((producto) => producto.id === id);
+    if (producto != undefined){
+        do {
+            opcion = prompt(`Seleccione una opción a modificar:
+            1.Modificar nombre
+            2.Modificar categoria
+            3.Modificar precio
+            99.Volver al menu pricipal`);
+                switch (opcion) {
+                    case "1":
+                        nombre = prompt("Ingrese nuevo nombre");
+                        producto.nombre = nombre;
+                        break;
+                    case "2":
+                    categoria = prompt (`Ingresar una de las categorias disponibles:
+                    1. Textil
+                    2. Iluminación
+                    3. Decoración
+                    4. Cuadros`);
+                    if (categoria == 1) {
+                        categoria = "Textil";
+                        producto.categoria = categoria;
+                    }
+                    else if (categoria == 2) {
+                        categoria = "Iluminación";
+                        producto.categoria = categoria;
+                    }
+                    else if (categoria == 3) {
+                        categoria = "Decoración";
+                        producto.categoria = categoria 
+                    }
+                    else if (categoria == 4) {
+                        categoria = "Cuadros";
+                        producto.categoria = categoria 
+                    }
+                    else 
+                        Alert ("Eliga la opción correcta");
+                        break;
+                    case "3":
+                        precio = prompt("Ingrese nuevo precio");
+                        producto.precio = precio
+                        break;
+                    case "99":
+                        break;
+                    default:
+                        alert("Opción inválida");
+                        break; 
+                }
+        }
+        while (opcion !== "99");
+        console.log(productos);
+    }
+    else{
+        alert("No existe el producto");
+    }
+    return
+}
+
+function eliminarProducto(id) {
+    let producto = this.productos.find((producto) => producto.id === id);
+    if (producto != undefined){
+        let index = this.productos.indexOf(producto);
+        this.productos.splice(index, 1);
+        alert("Producto quitado de stock");
+    }
+    else{
+        alert("No existe el producto");
+    }
+    return 
+}
+
+function administrarProductos(id) {
+    do {
+        opcion = prompt(`Seleccione una opción:
+        1. Agregar producto
+        2. Modificar producto
+        3. Listar productos  
+        4. Eliminar producto
+        99. Salir`);
+        switch (opcion) {
             case "1":
-                opcioncompra = prompt("\n 1) Almohadon 50 x 50   $4000 \n 2) Manta   $5000 \n 3) Alfombra   $6500 \n 9) Volver al menu anterior");
-                if (opcioncompra == "1"){
-                    descripción = "\n 1) Almohadon 50 x 50   $4000";
-                    Carrito(descripción);
+                nombre = prompt("Ingresar nombre del producto");
+                categoria = prompt (`Ingresar una de las categorias disponibles:
+                1. Textil
+                2. Iluminación
+                3. Decoración
+                4. Cuadros`);
+                precio = prompt ("Ingresar precio del producto");
+                if (categoria == 1) {
+                    categoria = "Textil";
+                    agregarProductos(id,nombre,categoria,precio);
+                    alert("Producto Agregado");
                 }
-                else if (opcioncompra == "2"){
-                        descripción = "\n 2) Manta   $5000";
-                        Carrito(descripción);
+                else if (categoria == 2) {
+                    categoria = "Iluminación";
+                    agregarProductos(id,nombre,categoria,precio);
+                    alert("Producto Agregado");
                 }
-                else if (opcioncompra == "3"){
-                        descripción = "\n 3) Alfombra   $6500";
-                        Carrito(descripción);
+                else if (categoria == 3) {
+                    categoria = "Decoración";
+                    agregarProductos(id,nombre,categoria,precio);
+                    alert("Producto Agregado");
                 }
-                else if (opcioncompra == "9"){
-                        break;
+                else if (categoria == 4) {
+                    categoria = "Cuadros";
+                    agregarProductos(id,nombre,categoria,precio);
+                    alert("Producto Agregado");
                 }
-                else if (opcioncompra != 1,2,3,9){
-                        alert ("La opción seleccionada no es correcta");
-                }
-            break;
+                else 
+                    Alert ("Eliga la opción correcta");
+                break;
             case "2":
-                opcioncompra = prompt("\n 1) Luminaria colgante   $10.000 \n 2) Luminaria de pie   $15.000 \n 3) Luminaria fibras naturales chica  $8.000 \n 9) Volver al menu anterior");
-                if (opcioncompra == "1") {
-                    descripción = "\n 1) Luminaria colgante   $10.000";
-                    Carrito(descripción);
-                }
-                else if (opcioncompra == "2") {
-                    descripción = "\n 2) Luminaria de pie   $15.000";
-                    Carrito(descripción);
-                }
-                else if (opcioncompra == "3") {
-                        descripción = "\n 3) Luminaria fibras naturales chica  $8.000";
-                        Carrito(descripción);
-                }
-                else if (opcioncompra == "9"){
-                    break;
-                }
-                else if (opcioncompra != 1,2,3,9) {
-                    alert ("La opción seleccionada no es correcta");
-                }
-            break; 
+                id = parseInt(prompt("Ingresar el codigo del producto a modificar"));
+                modificarProducto(id,productos);
+                alert("Producto Modificado");
+                break;
             case "3":
-                opcioncompra = prompt("\n 1) Floreros   $2000 \n 2) Velas   $1500 \n 3) Macetas   $2000 \n 9) Volver al menu anterior");
-                if (opcioncompra == "1") {
-                    descripción = "\n 1) Floreros   $2000";
-                    Carrito(descripción);
-                }
-                else if (opcioncompra == "2") {
-                        descripción = "\n 2) Velas   $1500";
-                        Carrito(descripción);
-                }
-                else if (opcioncompra == "3") {
-                    descripción = "\n 3) Macetas   $2000";
-                    Carrito(descripción);
-                }
-                else if (opcioncompra == "9") {
-                        break;
-                }
-                else if (opcioncompra != 1,2,3,9) {
-                    alert ("La opción seleccionada no es correcta");
-                }
+                alert(verProductos(productos));
                 break;
             case "4":
-                opcioncompra = prompt("\n 1) Cuadros chicos  $2000 \n 2) Cuadros medianos   $3500 \n 3)Cuadros grandes   $4500 \n 9) Volver al menu anterior");
-                if (opcioncompra == "1") {
-                    descripción = "\n 1) Cuadros chicos  $2000";
-                    Carrito(descripción);
+                id = parseInt(prompt("Ingresar codigo del producto a eliminar"));
+                eliminarProducto(id);
+                break;
+            case "99":
+                break;
+            default:
+                alert("Opción inválida");
+                break;
+        }
+    } while (opcion !== "99");
+    console.log(productos);
+}
+
+function menu () {
+    
+    var id = {id:1};
+    var password = {password:false};
+    productos = [];
+    filtrado = [];
+    precargararreglo(productos,id);
+    let opción = prompt (`Seleccione el filtro deseado:
+                1. Textil
+                2. Iluminación
+                3. Decoración
+                4. Cuadros
+                5. Ver Todos Los Articulos
+                98. Administrar Tienda
+                99. Salir`)
+    while (opción != 99) {
+        switch (opción) {
+        case "1":
+            filtrarProductos("Textil");
+            opcioncompra = parseInt(prompt("Elija un Articulo de la  " + (verProductos(resultado))));
+            existe = ExisteArticulo(resultado,opcioncompra);
+            if (existe == true) {
+                Carrito(verProducto(opcioncompra));
+                eliminarProducto(opcioncompra);
+            }
+            else {
+                alert("Opcion Incorrecta");
+            }
+        break;
+        case "2":
+            filtrarProductos("Iluminación");
+            opcioncompra = parseInt(prompt("Elija un Articulo de la  " + (verProductos(resultado))));
+            existe = ExisteArticulo(resultado,opcioncompra);
+            if (existe == true) {
+                Carrito(verProducto(opcioncompra));
+                eliminarProducto(opcioncompra);
+            }
+            else {
+                alert("Opcion Incorrecta");
+            }
+            break; 
+            case "3":
+            filtrarProductos("Decoración");
+            opcioncompra = parseInt(prompt("Elija un Articulo de la  " + (verProductos(resultado))));
+            existe = ExisteArticulo(resultado,opcioncompra);
+            if (existe == true) {
+                Carrito(verProducto(opcioncompra));
+                eliminarProducto(opcioncompra);
+            }
+            else {
+                alert("Opción Incorrecta");
+            }
+            break;
+            case "4":
+                filtrarProductos("Cuadros");
+            opcioncompra = parseInt(prompt("Elija un Articulo de la  " + (verProductos(resultado))));
+            existe = ExisteArticulo(resultado,opcioncompra);
+            if (existe == true) {
+                Carrito(verProducto(opcioncompra));
+                eliminarProducto(opcioncompra);
+            }
+            else {
+                alert("Opción Incorrecta");
+            }
+            break;
+            case "5": {
+                alert(verProductos(productos));
+            }
+            break;
+            case "98":{
+                contraseña(password);
+                if (password.password == true) {
+                    administrarProductos(id);
                 }
-                else if (opcioncompra == "2") {
-                    descripción = "\n 2) Cuadros medianos   $3500";
-                    Carrito(descripción);
-                }
-                else if (opcioncompra == "3") {
-                        descripción = "\n 3) Cuadros grandes   $4500";
-                        Carrito(descripción);
-                }
-                else if (opcioncompra == "9"){
-                    break;
-                }
-                else if (opcioncompra != 1,2,3,9) {
-                    alert ("La opción seleccionada no es correcta");
+                else {
+                    alert ("La contraseña es incorrecta");
                 }
                 break;
+            }
             default: 
                 alert ("La opción seleccionada no es correcta");
                 break;
             }
-    opción = prompt ("Seleccione la opción deseada\n 1) Textil \n 2) Iluminación \n 3) Decoración \n 4) Cuadros \n 0) Salir");
+    opción = prompt ("Seleccione la opción deseada\n 1. Textil \n 2. Iluminación \n 3. Decoración \n 4. Cuadros \n 5. Ver todos los Artículos \n 98. Administrar Tienda \n 99) Salir");
     }  
 }
 
@@ -109,5 +322,6 @@ function Saludo () {
 
 
 Bienvenida ();
+
 menu ();
 Saludo ();
